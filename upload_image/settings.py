25edafa6125678d4f40a05cb8dee5016c4e39c9a
Erickson Lopes <erickson-lopes-dev>
2020-09-28 19:04:57 -0300

    deploy

--- upload_image/settings.py
+++ upload_image/settings.py
@@ -1,5 +1,7 @@
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -113,3 +115,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+
+django_heroku.settings(locals())
